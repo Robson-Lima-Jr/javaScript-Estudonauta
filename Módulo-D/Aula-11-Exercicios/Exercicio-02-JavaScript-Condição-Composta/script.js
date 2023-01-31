cb70703363499e9d55fa1c54868d0959a20b4394
Robson-Jr <robsonjuniorsn@gmail.com>
2023-01-31 17:58:58 -0300

    exercicio 02, concluido

---
+++ Módulo-D/Aula-11-Exercicios/Exercicio-02-JavaScript-Condição-Composta/script.js
@@ -0,0 +1,7 @@
+let nascionalidade = 'Português';
+
+if(nascionalidade === 'Brasileiro') {
+    console.log(`Você é ${nascionalidade}`);
+} else {
+    console.log(`Você é ${nascionalidade}, portanto é Estrangeiro`);
+}
