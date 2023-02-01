8b005ca78f13f9296299b2d901441d69dfa90a96
Robson-Jr <robsonjuniorsn@gmail.com>
2023-02-01 18:34:04 -0300

    Exercicio 02, aula 12, concluido

--- Módulo-D/Aula-12-Exercicios/Exercicio-02-Condição-Aninhada-Hora-do-Dia/script.js
+++ Módulo-D/Aula-12-Exercicios/Exercicio-02-Condição-Aninhada-Hora-do-Dia/script.js
@@ -4,6 +4,14 @@ let horas = time.getHours();
 let minutos = time.getMinutes();
 let segundos = time.getSeconds();
 
-console.log(horas);
-console.log(minutos);
-console.log(segundos);
+let horarioAtual = `${horas} Horas : ${minutos} Minutos : ${segundos} Segundos`;
+
+if(horas >= 0 && horas < 5) {
+    console.log(`Boa Madrugada, agora são ${horarioAtual}`);
+} else if (horas >= 5 && horas < 12){
+    console.log(`Bom Dia, agora são ${horarioAtual}`);
+} else if (horas >= 12 && horas < 18 ) {
+    console.log(`Boa Tarde, agora são ${horarioAtual}`);
+} else {
+    console.log(`Boa Noite, agora são ${horarioAtual}`);
+}
