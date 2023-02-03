e7a6e47d4ed585c4fe10ab2cfcebf8345d3c22ea
Robson-Jr <robsonjuniorsn@gmail.com>
2023-02-03 17:46:54 -0300

    Exercicio especial, concluido

--- Módulo-D/Exercicios-Especiais/Exercicio-Hora-do-Dia/script.js
+++ Módulo-D/Exercicios-Especiais/Exercicio-Hora-do-Dia/script.js
@@ -4,12 +4,40 @@ function timeChange() {
     let minutos = data.getMinutes();
     let segundos = data.getSeconds();
 
+
     let timeRes = document.querySelector("div#txtHr");
-    let imageLoad = document.querySelector("div#img");
+    let faixa = document.querySelector("div#faixaDia");
+    let imageLoad = document.querySelector("img#imagem");
+    let h1 = document.querySelector("h1");
+    let footer = document.querySelector("footer");
 
-    let finalRes = `${horas} Horas, ${minutos} minutos e ${segundos}.`;
+    timeRes.innerHTML = `<strong>Agora são:</strong> ${horas} Horas, ${minutos} minutos e ${segundos} segundos.`;
 
-    
+    if (horas >= 0 && horas < 5) {
+        imageLoad.src = "/Módulo-D/Exercicios-Especiais/Exercicio-Hora-do-Dia/imagens/madrugada.png";
 
-    
+        document.body.style.background = "#1a3950ff";
+
+        faixa.innerHTML = "Boa Madrugada";
+    } else if (horas >= 5 && horas < 12) {
+        imageLoad.src = "/Módulo-D/Exercicios-Especiais/Exercicio-Hora-do-Dia/imagens/manhã.png";
+
+        document.body.style.background = "#f7cd96ff";
+        h1.style.color = "black";
+        footer.style.color = "Black";
+
+        faixa.innerHTML = "Bom Dia";
+    } else if (horas >= 12 && horas < 18) {
+        imageLoad.src = "/Módulo-D/Exercicios-Especiais/Exercicio-Hora-do-Dia/imagens/tarde.png";
+
+        faixa.innerHTML = "Boa Tarde";
+    } else {
+        imageLoad.src = "/Módulo-D/Exercicios-Especiais/Exercicio-Hora-do-Dia/imagens/noite.png";
+
+        document.body.style.background = "#152d37ff"
+
+        faixa.innerHTML = "Boa Noite";
+    }
+
+
 }
