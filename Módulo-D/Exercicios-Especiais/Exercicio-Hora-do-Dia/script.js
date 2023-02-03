57b98f6240986093ac400115e172a349a67620db
Robson-Jr <robsonjuniorsn@gmail.com>
2023-02-03 16:46:49 -0300

    Exercicio especial atualizado

--- Módulo-D/Exercicios-Especiais/Exercicio-Hora-do-Dia/script.js
+++ Módulo-D/Exercicios-Especiais/Exercicio-Hora-do-Dia/script.js
@@ -1,1 +1,15 @@
-window.alert('oi');
+function timeChange() {
+    let data = new Date();
+    let horas = data.getHours();
+    let minutos = data.getMinutes();
+    let segundos = data.getSeconds();
+
+    let timeRes = document.querySelector("div#txtHr");
+    let imageLoad = document.querySelector("div#img");
+
+    let finalRes = `${horas} Horas, ${minutos} minutos e ${segundos}.`;
+
+    
+
+    
+}
