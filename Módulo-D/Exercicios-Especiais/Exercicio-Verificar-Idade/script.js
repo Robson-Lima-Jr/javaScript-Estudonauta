ee0e6034314bfa091ae548ad5fa4c6168612749f
Robson-Jr <robsonjuniorsn@gmail.com>
2023-02-06 18:12:04 -0300

    ExerEsp02, atualizado

---
+++ Módulo-D/Exercicios-Especiais/Exercicio-Verificar-Idade/script.js
@@ -0,0 +1,8 @@
+let btnClick = document.querySelector("input#btn");
+
+btnClick.addEventListener("click", isClicked);
+
+function isClicked() {
+    let nasc = document.querySelector("input#txtNum");
+    
+}
