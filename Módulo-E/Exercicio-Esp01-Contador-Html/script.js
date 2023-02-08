04efb5685ad786b7327dc0fbfe4e8f0e0f2b372a
Robson-Jr <robsonjuniorsn@gmail.com>
2023-02-08 18:52:32 -0300

    ExerEsp01, moduloE, concluido(esqueci de fazer commit no meio do caminho)

--- Módulo-E/Exercicio-Esp01-Contador-Html/script.js
+++ Módulo-E/Exercicio-Esp01-Contador-Html/script.js
@@ -10,4 +10,27 @@ function isClicked() {
     let start = Number(getStart.value);
     let end = Number(getEnd.value);
     let cont = Number(getCont.value);
+
+    if (getStart.value.length == 0 || getEnd.value.length == 0 || getCont.value.length == 0) {
+        res.innerHTML = `[ERRO] dados faltando, impossivel resolver`;
+        res.innerHTML += `\u{1F6AB}`
+    } else {
+        res.innerHTML = `<strong>Contando: </strong><br>`
+        if (cont <= 0) {
+            window.alert("Impossivel prosseguir com o Passo = 0. O valor de passo será automaticamente definido em 1");
+            cont = 1;
+        }
+
+        if(end > start) {
+            for (let index = start; index <= end; index += cont) {
+                res.innerHTML += `${index} \u{1F449}`;
+            } 
+        } else if (start > end){
+            for (let index = start; index >= end; index -= cont) {
+                res.innerHTML += `${index} \u{1F449}`;
+            } 
+            }else{
+                res.innerHTML += `Impossivel Resolver com valores de Inicio e fim iguais \u{26A0}`
+            }res.innerHTML += `\u{2705}`
+    }
 }
