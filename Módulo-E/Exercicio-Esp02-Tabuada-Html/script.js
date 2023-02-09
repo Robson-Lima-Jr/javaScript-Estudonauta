afb17fa3cef7231fefe41af8a72588fdd94ac592
Robson-Jr <robsonjuniorsn@gmail.com>
2023-02-08 21:12:25 -0300

    Exercicio especial02, moduloE, concluido

--- Módulo-E/Exercicio-Esp02-Tabuada-Html/script.js
+++ Módulo-E/Exercicio-Esp02-Tabuada-Html/script.js
@@ -1,16 +1,23 @@
 let btnClick = document.querySelector("input#btn");
 btnClick.addEventListener("click", isClicked);
 
-function isClicked() {  
-    let res = document.querySelector("div#res");
+function isClicked() {
     let getNumber = document.querySelector("input#txtNum");
+    let addSelectItem = document.querySelector("select#selTab");
 
     let num = Number(getNumber.value);
     let mult = 10;
 
-    res.innerHTML = `Tabuada do Número: ${num}`;
-
-    for (let index = 0; index <= mult; index++) {
-        res.innerHTML += `<p>${num} x ${index} = ${num*index}`   
+    if (getNumber.value.length == 0) {
+        window.alert("Sem valor dectado, favor digitar um número")
+    } else {
+        addSelectItem.innerHTML = "";
+        for (let i = 0; i <= mult; i++) {
+            let item = document.createElement("option");
+            item.text = `${num} x ${i} = ${num * i}`;
+            item.value = `tab${i}`;
+            addSelectItem.appendChild(item);
+        }
     }
+
 }
