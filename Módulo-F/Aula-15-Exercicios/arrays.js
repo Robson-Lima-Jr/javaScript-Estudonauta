4479c86fe8cebc2f78f0d10d537cfc2493465029
Robson-Jr <robsonjuniorsn@gmail.com>
2023-02-09 19:53:36 -0300

    Nome do arquivo alterado e parte01 concluida

---
+++ Módulo-F/Aula-15-Exercicios/arrays.js
@@ -0,0 +1,12 @@
+//Parte 01 - Mostrar o modo simples de Arrays
+let base = [10,21,35,78,44,3];
+
+console.log(`------PARTE 01------`);
+console.log(base);
+console.log(base[0]);
+console.log(base[1]);
+console.log(base[2]);
+console.log(base[3]);
+console.log(base[4]);
+console.log(base[5]);
+console.log(``);
