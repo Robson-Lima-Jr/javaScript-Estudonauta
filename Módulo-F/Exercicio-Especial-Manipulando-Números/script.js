200186bcafcf720dcf45b9a18022f284ec12b860
Robson-Jr <robsonjuniorsn@gmail.com>
2023-02-13 19:38:28 -0300

    ExerEsp-moduloF, atualizado, funçao para o primeiro botão

--- Módulo-F/Exercicio-Especial-Manipulando-Números/script.js
+++ Módulo-F/Exercicio-Especial-Manipulando-Números/script.js
@@ -1,12 +1,19 @@
 let btnAdd = document.querySelector("input#btn1");
 
-btnAdd.addEventListener("click" , addNumero);
+btnAdd.addEventListener("click", addNumero);
 
-function addNumero () {
+function addNumero() {
     let getNum = document.querySelector("input#numero");
     let num = Number(getNum.value);
 
-    if(getNum.value.length == 0) {
-        window.alert("Valor inválido ou já Adicionado na Lista")
+    if (getNum.value.length == 0 || num > 100 || num <= 0) {
+        window.alert("Valor inválido ou já Adicionado na Lista");
+    } else {
+        let selectRes = document.querySelector("select#arrayNum");
+        
+        let addItem = document.createElement("option");
+
+        addItem.text = `Valor Adicionado: ${num}`;
+        selectRes.appendChild(addItem);
     }
 }
