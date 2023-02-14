0929808319ca54a4a5efad453e6dd47ad008a221
Robson-Jr <robsonjuniorsn@gmail.com>
2023-02-14 15:28:48 -0300

    Exercicio especial concluido e readme.md atualizado

--- Módulo-F/Exercicio-Especial-Manipulando-Números/script.js
+++ Módulo-F/Exercicio-Especial-Manipulando-Números/script.js
@@ -21,17 +21,17 @@ function numbersValid(valid) {
     }
 }
 
-function repeatedNumbers (rpt1, rpt2){
-    if(rpt2.indexOf(Number(rpt1)) != -1){
+function repeatedNumbers(rpt1, rpt2) {
+    if (rpt2.indexOf(Number(rpt1)) != -1) {
         return true;
-    }else {
+    } else {
         return false;
     }
 }
 
 //Função para o clique no botão 1, que adiciona valores
 function addNumero() {
-    if(numbersValid(num.value) && !repeatedNumbers(num.value, valores)){
+    if (numbersValid(num.value) && !repeatedNumbers(num.value, valores)) {
         valores.push(Number(num.value));
         let itemList = document.createElement("option");
         itemList.text = `Valor adicionado: ${num.value}`;
@@ -45,25 +45,32 @@ function addNumero() {
 }
 
 function finalizar() {
-    if(valores.length == 0) {
+    if (valores.length == 0) {
         window.alert("Sem valores para Finalizar o programa, favor adicione algum(s).")
     } else {
         let tot = valores.length;
         let maior = valores[0];
         let menor = valores[0];
+        let soma = 0;
+        let media = 0;
 
-        for(let i in valores){
-            if (valores[i] > maior){
+        for (let i in valores) {
+            soma += valores[i];
+
+            if (valores[i] > maior) {
                 maior = valores[i];
-            } if (valores[i] < menor){
+            } if (valores[i] < menor) {
                 menor = valores[i];
             }
         }
+        media = soma / tot ;
 
         res.innerHTML = ``;
         res.innerHTML += `<P> A quantidade de números digitados foi: ${tot}`;
         res.innerHTML += `<p> O maior valor digitado foi: ${maior}`;
         res.innerHTML += `<p> O menor valor digitado foi: ${menor}`;
+        res.innerHTML += `<p> A soma dos valores digitados foi: ${soma}`;
+        res.innerHTML += `<p> A média dos valores digitados foi: ${media.toFixed(2)}`;
     }
 
 }
